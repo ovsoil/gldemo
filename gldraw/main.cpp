8a0161064a345d829cdfbec92d26f88e86dee82f
ovsoil <hxyumail@126.com>
2014-11-21 01:09:05 +0800

    Initial Commit

---
+++ gldraw/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gldraw
+//
+//  Created by ovsoil on 11/21/14.
+//  Copyright (c) 2014 ___XD___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
